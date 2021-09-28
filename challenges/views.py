dc17838250043ef8f0cf46bf74bb2260f964ad5e
Matt King <mattkingit@gmail.com>
2021-09-28 10:19:01 -0500

    Adding some HTML programmatically

--- challenges/views.py
+++ challenges/views.py
@@ -18,6 +18,16 @@ monthly_challenges = {
     "december":"This is December"
 }
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"   
+    return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -37,5 +47,3 @@ def monthly_challenge(request, month):
     except:
         return HttpResponseNotFound("This month is not supported")
     
-
-
